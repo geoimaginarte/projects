9aec280f73d4a267fe1d30d66aab40838b5e31c1
Geoimaginarte <frgeoimagen@gmail.com>
2020-03-16 11:25:22 -0600

    update

---
+++ prospectiva/toptalent/data/Oficentros_3.js
@@ -0,0 +1,14 @@
+var json_Oficentros_3 = {
+"type": "FeatureCollection",
+"name": "Oficentros_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "Tipo": "Oficentros", "Categorias": "A+", "Nombre": "Top Talent", "x": -84.344741666666664, "y": 10.046286111111112 }, "geometry": { "type": "Point", "coordinates": [ -84.344741666666664, 10.046286111111112 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "Tipo": "Oficentros", "Categorias": "A+", "Nombre": "Universal", "x": -84.098919444444434, "y": 9.9319805555555547 }, "geometry": { "type": "Point", "coordinates": [ -84.098919444444434, 9.931980555555555 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "Tipo": "Oficentros", "Categorias": "A", "Nombre": "Forum 2", "x": -84.194561111111113, "y": 9.9557111111111105 }, "geometry": { "type": "Point", "coordinates": [ -84.194561111111113, 9.955711111111111 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "Tipo": "Oficentros", "Categorias": "B", "Nombre": "Europlaza", "x": -84.154233333333337, "y": 9.9826055555555548 }, "geometry": { "type": "Point", "coordinates": [ -84.154233333333337, 9.982605555555555 ] } },
+{ "type": "Feature", "properties": { "fid": "5", "Tipo": "Oficentros", "Categorias": "A+", "Nombre": "Cedral", "x": -84.131463888888888, "y": 9.9348194444444449 }, "geometry": { "type": "Point", "coordinates": [ -84.131463888888888, 9.934819444444445 ] } },
+{ "type": "Feature", "properties": { "fid": "6", "Tipo": "Oficentros", "Categorias": "B", "Nombre": "Centro Corporativo del Este", "x": -84.071197222222224, "y": 9.9460305555555557 }, "geometry": { "type": "Point", "coordinates": [ -84.071197222222224, 9.946030555555556 ] } },
+{ "type": "Feature", "properties": { "fid": "7", "Tipo": "Oficentros", "Categorias": "A", "Nombre": "Terra Campus", "x": -83.994505555555563, "y": 9.901508333333334 }, "geometry": { "type": "Point", "coordinates": [ -83.994505555555563, 9.901508333333334 ] } }
+]
+}
