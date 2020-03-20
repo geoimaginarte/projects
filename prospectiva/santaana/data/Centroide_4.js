f159e8f43ef6891f1535f71c5a06f5c70036ea77
Geoimaginarte <frgeoimagen@gmail.com>
2020-03-20 17:59:44 -0600

    new

---
+++ prospectiva/santaana/data/Centroide_4.js
@@ -0,0 +1,8 @@
+var json_Centroide_4 = {
+"type": "FeatureCollection",
+"name": "Centroide_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "Longitud": -84.187638888888898, "Latitud": 9.9269833333333324 }, "geometry": { "type": "Point", "coordinates": [ -84.187638888888898, 9.926983333333332 ] } }
+]
+}
