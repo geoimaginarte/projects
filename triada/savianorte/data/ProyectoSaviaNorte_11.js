f624de933c5be99c8a1d5f224567adf917d33e1b
jfran <frgeoimagen@gmail.com>
2021-11-08 19:19:57 -0600

    Create HTML

---
+++ triada/savianorte/data/ProyectoSaviaNorte_11.js
@@ -0,0 +1,1 @@
+var json_ProyectoSaviaNorte_11 = {"type":"FeatureCollection","name":"ProyectoSaviaNorte_11","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null,"Nombre":"Savia Norte"},"geometry":{"type":"Point","coordinates":[-84.147076366650239,9.973318689706007]}}]}
